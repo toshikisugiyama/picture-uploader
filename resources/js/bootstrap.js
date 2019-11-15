f7c97ebf65c28210379f9e3519059bfaeb153981
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-11-15 22:33:34 +0900

    implement CSRF prevention for Web API

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,3 +26,10 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
+
+import {getCookieValue} from './util';
+
+window.axios.interceptors.request.use(config => {
+  config.headers['X-XSRF-TOKEN'] = getCookieValue('XSRF-TOKEN');
+  return config;
+});
